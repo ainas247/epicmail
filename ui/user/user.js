e3e9deba255861bb5fc40542836129cbf0c35d18
Emmanuel Michael <ainas247@gmail.com>
2019-03-13 11:53:09 +0100

    User should be able to retreive email sent should have page where user can retreive sent mail

---
+++ ui/user/user.js
@@ -0,0 +1,14 @@
+document.getElementById("displaymails").style.display = "none";
+document.getElementById("displaymails2").style.display = "none";
+
+function displayMails() {
+    document.getElementById("displaymails").style.display = "inline-block";
+    document.getElementById("displaymails2").style.display = "none";
+}
+
+function displayMails2() {
+    document.getElementById("displaymails2").style.display = "inline-block";
+    document.getElementById("displaymails").style.display = "none";
+
+}
+
