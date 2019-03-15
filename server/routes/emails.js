1e39278da6785f9af0438bbd0e56fade34724c82
Emmanuel Michael <ainas247@gmail.com>
2019-03-15 02:37:59 +0100

    User should get specifc email

--- server/routes/emails.js
+++ server/routes/emails.js
@@ -7,4 +7,13 @@ router.get("/", (req, res) => {
   res.send(emails);
 });
 
+//GET MESSAGE ID 
+router.get('/:id', (req, res) => {
+  let findMessage = emails.find((email) => email.id === parseInt(req.params.id))
+  if (!findMessage) res.status('404').send('Message Not found');
+  res.status(200).send(findMessage);
+})
+
 module.exports = router;
+
+
