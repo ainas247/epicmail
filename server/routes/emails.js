64d3d8afefe72ccdb5f3dd56ae64462b3a0edb91
Emmanuel Michael <ainas247@gmail.com>
2019-03-15 03:59:10 +0100

    adding updating email file

--- server/routes/emails.js
+++ server/routes/emails.js
@@ -31,14 +31,14 @@ router.post('/', (req, res) => {
   }
   emails.push(email)
   res.send(email)
-=======
+
 //GET MESSAGE ID 
 router.get('/:id', (req, res) => {
   let findMessage = emails.find((email) => email.id === parseInt(req.params.id))
   if (!findMessage) res.status('404').send('Message Not found');
   res.status(200).send(findMessage);
-=======
-//GET MESSAGE SENT / DRAFT AND UNREAD MESSAGE
+
+  //GET MESSAGE SENT / DRAFT AND UNREAD MESSAGE
 router.get('/status/:status', (req, res) => {
     let emailStatus = emails.filter(message => message.status === req.params.status)
     if (!emailStatus) return res.status("404").send("No message found");
@@ -46,5 +46,3 @@ router.get('/status/:status', (req, res) => {
 })
 
 module.exports = router;
-
-
