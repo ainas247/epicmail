86bb13205231e79822413a551173a5f6004cb58a
Emmanuel Michael <ainas247@gmail.com>
2019-03-15 02:16:08 +0100

    fux bug user should get all mail

--- server/routes/emails.js
+++ server/routes/emails.js
@@ -7,4 +7,11 @@ router.get("/", (req, res) => {
   res.send(emails);
 });
 
+//GET MESSAGE SENT / DRAFT AND UNREAD MESSAGE
+router.get('/status/:status', (req, res) => {
+    let emailStatus = emails.filter(message => message.status === req.params.status)
+    if (!emailStatus) return res.status("404").send("No message found");
+        res.send(emailStatus);
+})
+
 module.exports = router;
